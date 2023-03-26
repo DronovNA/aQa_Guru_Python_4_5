5144413273f7e83863b3bc48a0387b3037b9919f
DronovNA <nikita.dronov.a@gmail.com>
2023-03-26 23:06:31 +0300

    1

---
+++ Test/test_demoqa_refactor.py
@@ -0,0 +1,43 @@
+from selene import browser
+from selene import be, have
+import os
+
+
+def test_form(open_browser_form):
+    browser.element("#firstName").should(be.blank).type("Test").press_enter()
+    browser.element("#lastName").should(be.blank).type("Testovich")
+    browser.element("#userEmail").should(be.blank).type("Test@example.com")
+    browser.element('[for="gender-radio-2"]').should(be.clickable).click()
+    browser.element("#userNumber").should(be.blank).type("89997589856")
+    browser.execute_script("window.scrollBy(0, 500)")
+    browser.element("#dateOfBirthInput").should(be.clickable).click()
+    browser.element('[value="1998"]').should(be.clickable).click()
+    browser.element('[class="react-datepicker__day react-datepicker__day--001 react-datepicker__day--weekend"]').should(
+        be.clickable
+    ).click()
+    browser.element("#subjectsInput").should(be.blank).type("sci").press_enter()
+    browser.element('[for="hobbies-checkbox-2"]').should(be.clickable).click()
+    browser.element("#uploadPicture").send_keys(os.getcwd() + "/example.txt")
+    browser.element("#currentAddress").should(be.blank).type("Test test test")
+    browser.element("#state").should(be.clickable).click()
+    browser.element('//div[text()="NCR"]').should(be.clickable).click()
+    browser.element("#city").should(be.clickable).click()
+    browser.element('//div[text()="Delhi"]').should(be.clickable).click()
+    browser.element("#submit").submit()
+    browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
+
+    # check_result
+    browser.all(".table-responsive td").by(
+        have.exact_texts(
+            "Test Testovich"
+            "Test@example.com"
+            "Female"
+            "8999758985"
+            "01 March,1998"
+            "Computer Science"
+            "Reading"
+            "example.txt"
+            "Test test test"
+            "NCR Delhi"
+        )
+    )
